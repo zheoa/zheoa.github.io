e97b83aaff0de762c27017f71e3feee0ae574f68
zheoa <domnic.redmi4@gmail.com>
2026-09-12 06:31:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@
 // ============================================================
 
 window.ZHEOA_CONFIG = {
-  SUPABASE_URL: 'PASTE_YOUR_PROJECT_URL_HERE',
-  SUPABASE_ANON_KEY: 'PASTE_YOUR_ANON_KEY_HERE'
+  SUPABASE_URL: 'https://xdmnrnidlyhytwrlybzs.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhkbW5ybmlkbHloeXR3cmx5YnpzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNTU1NTMsImV4cCI6MjEwNDczMTU1M30.ITAWkaLPFE-GaMCH-40yOk9bGRxQKL_Q4b7ljAuBqtg'
 };
